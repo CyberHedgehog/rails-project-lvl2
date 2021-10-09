99b842f32c973cf8630b1ad7e6a75d90318ca39f
dmitriy <d.dobrenkiy@hotmail.com>
2021-10-10 00:31:10 +0300

    locales

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
   root 'home#index'
-  devise_for :users, controllers: { 
+  devise_for :users, controllers: {
     registrations: 'users/registrations',
     sessions: 'users/sessions'
   }
