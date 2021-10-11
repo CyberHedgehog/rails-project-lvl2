62843381541ac3e5c557997e1f6b201ec0e9fecd
dmitriy <d.dobrenkiy@hotmail.com>
2021-10-11 20:55:11 +0300

    post index, show

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
   resources :posts
-  root 'home#index'
+  root 'posts#index'
   resources :post_categories, only: :index
   devise_for :users, controllers: {
     registrations: 'users/registrations',
