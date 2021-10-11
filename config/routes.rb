abb0cb9ca68b89bd53b2082cce7c669d28214283
dmitriy <d.dobrenkiy@hotmail.com>
2021-10-11 20:20:41 +0300

    feat: ppost, post_category resources

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,7 @@
 Rails.application.routes.draw do
+  resources :posts
   root 'home#index'
+  resources :post_categories, only: :index
   devise_for :users, controllers: {
     registrations: 'users/registrations',
     sessions: 'users/sessions'
