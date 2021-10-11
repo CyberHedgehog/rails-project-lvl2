58380607acbedbefc3cd48edcea817828b27d3f5
dmitriy <d.dobrenkiy@hotmail.com>
2021-10-11 20:29:56 +0300

    categories seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,6 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+%i[music movies books].each do |item|
+  PostCategory.create(name: item)
+end
