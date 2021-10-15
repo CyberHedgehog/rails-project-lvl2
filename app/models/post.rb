edee8749570cf71687561266b040e58151ab5f9c
Dmitriy <reg.dobr@live.ru>
2021-10-15 16:30:50 +0300

    fix model

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,5 +1,5 @@
 class Post < ApplicationRecord
   belongs_to :user
   belongs_to :post_category
-  has_many :comments, class_name: 'PostComment'
-end 
+  has_many :comments, class_name: 'PostComment', dependent: :restrict_with_exception
+end
