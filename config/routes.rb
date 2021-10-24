97c10b5a5add1dc76eb30cfdace3eda27db114cc
dmitriy <d.dobrenkiy@hotmail.com>
2021-10-24 20:16:08 +0300

    feat: likes

--- config/routes.rb
+++ config/routes.rb
@@ -4,6 +4,7 @@ Rails.application.routes.draw do
   resources :posts do
     scope module: :posts do
       resources :comments, only: %i[create]
+      resources :likes, only: %i[create destroy]
     end
   end
 
