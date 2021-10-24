46bb14a6b7f638e3c37c76f3dae7d0f58d9d272c
dmitriy <d.dobrenkiy@hotmail.com>
2021-10-24 20:29:00 +0300

    feat: bootstrap-icons

--- app/models/post.rb
+++ app/models/post.rb
@@ -2,4 +2,5 @@ class Post < ApplicationRecord
   belongs_to :user
   belongs_to :post_category
   has_many :comments, class_name: 'PostComment', dependent: :restrict_with_exception
+  has_many :likes, class_name: 'PostLike', dependent: :restrict_with_exception
 end
