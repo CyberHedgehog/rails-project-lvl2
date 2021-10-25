60249712504d7978f5c84d568beb46e5270fb5a9
Dmitriy <reg.dobr@live.ru>
2021-10-25 11:00:37 +0300

    feat: controller

--- app/controllers/posts/likes_controller.rb
+++ app/controllers/posts/likes_controller.rb
@@ -1,2 +1,12 @@
 class Posts::LikesController < ApplicationController
+  def create
+    PostLike.create(post_id: params[:post_id], user_id: current_user[:id])
+    redirect_to post_path(params[:post_id])
+  end
+
+  def destroy
+    like = PostLike.where(user_id: current_user.id, post_id: params[:post_id]).first
+    like.destroy
+    redirect_to post_path(params[:post_id])
+  end
 end
