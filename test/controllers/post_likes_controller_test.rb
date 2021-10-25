f0caf93d455933bf73d97cc2da426493bdde965a
dmitriy <d.dobrenkiy@hotmail.com>
2021-10-25 21:09:03 +0300

    feat(likes): test

--- test/controllers/post_likes_controller_test.rb
+++ test/controllers/post_likes_controller_test.rb
@@ -1,7 +1,24 @@
 require "test_helper"
 
 class PostLikesControllerTest < ActionDispatch::IntegrationTest
-  # test "the truth" do
-  #   assert true
-  # end
+  include Devise::Test::IntegrationHelpers
+  setup do
+    @post = posts(:one)
+    sign_in users(:one)
+    @like = post_likes(:one)
+  end
+
+  test 'should like post' do
+    assert_difference('PostLike.count') do
+      post post_likes_path(@post)
+    end
+    assert_redirected_to post_path(@post)
+  end
+
+  test 'should unlike post' do
+    assert_difference('PostLike.count', -1) do
+      delete post_like_path(post_id: @post.id, id: @like.id)
+    end
+    assert_redirected_to post_path(@post)
+  end
 end
