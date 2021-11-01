80ffb31b8e343aefdd66ad65dace39583dec5338
dmitriy <d.dobrenkiy@hotmail.com>
2021-11-01 11:35:30 +0300

    authorize to like

--- app/controllers/posts/likes_controller.rb
+++ app/controllers/posts/likes_controller.rb
@@ -1,4 +1,5 @@
 class Posts::LikesController < ApplicationController
+  before_action :authenticate_user!, only: %i[create destroy]
   def create
     PostLike.create(post_id: params[:post_id], user_id: current_user[:id]) if user_signed_in?
     redirect_to post_path(params[:post_id])
