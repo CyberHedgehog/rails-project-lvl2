2890a3e9379e5fa18e7bb178f1e8fa0719fc5f50
dmitriy <d.dobrenkiy@hotmail.com>
2021-11-08 22:31:17 +0300

    crefactor(likes): heck ing signed user

--- app/controllers/posts/likes_controller.rb
+++ app/controllers/posts/likes_controller.rb
@@ -1,15 +1,13 @@
 class Posts::LikesController < ApplicationController
   before_action :authenticate_user!, only: %i[create destroy]
   def create
-    PostLike.create(post_id: params[:post_id], user_id: current_user[:id]) if user_signed_in?
+    PostLike.create(post_id: params[:post_id], user_id: current_user.id)
     redirect_to post_path(params[:post_id])
   end
 
   def destroy
-    if user_signed_in?
-      like = PostLike.where(user_id: current_user.id, post_id: params[:post_id]).first
-      like.destroy
-    end
+    like = PostLike.where(user_id: current_user.id, post_id: params[:post_id]).first
+    like.destroy
     redirect_to post_path(params[:post_id])
   end
 end
