c7d2887b8f79bcae683db53cb09f0b07d2e76de4
dmitriy <d.dobrenkiy@hotmail.com>
2021-11-08 22:37:56 +0300

    refactor(comment): authenticate_user

--- app/controllers/posts/comments_controller.rb
+++ app/controllers/posts/comments_controller.rb
@@ -1,10 +1,7 @@
 class Posts::CommentsController < ApplicationController
+  before_action :authenticate_user!
   def create
-    unless user_signed_in?
-      redirect_to root_path
-      return
-    end
-    @comment = PostComment.new(comment_params.merge(user_id: current_user[:id], post_id: params[:post_id]))
+    @comment = PostComment.new(comment_params.merge(user_id: current_user.id, post_id: params[:post_id]))
     if @comment.save
       redirect_to post_path(params[:post_id])
     else
