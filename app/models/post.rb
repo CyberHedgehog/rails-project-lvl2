aef83e78f46c0dad5e74660e0589e7342f3d24ff
dmitriy <d.dobrenkiy@hotmail.com>
2021-11-10 21:54:44 +0300

    post model associations

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,8 +1,8 @@
 class Post < ApplicationRecord
   belongs_to :creator, class_name: 'User'
   belongs_to :post_category
-  has_many :comments, class_name: 'PostComment', dependent: :restrict_with_exception
-  has_many :likes, class_name: 'PostLike', dependent: :restrict_with_exception
+  has_many :comments, class_name: 'PostComment', dependent: :destroy
+  has_many :likes, class_name: 'PostLike', dependent: :destroy
 
   validates :title, :body, :post_category_id, presence: true
 end
