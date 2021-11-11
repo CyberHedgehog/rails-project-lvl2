d16c1503eb4d9f9436466b58113fd4563411110e
Dmitriy <reg.dobr@live.ru>
2021-11-11 08:25:11 +0300

    refactor: creating comments

--- app/controllers/posts/comments_controller.rb
+++ app/controllers/posts/comments_controller.rb
@@ -1,11 +1,11 @@
 class Posts::CommentsController < ApplicationController
   before_action :authenticate_user!
   def create
-    @comment = PostComment.new(comment_params.merge(user_id: current_user.id, post_id: params[:post_id]))
+    @post = Post.find(params[:post_id])
+    @comment = @post.comments.new(user_id: current_user.id, **comment_params)
     if @comment.save
       redirect_to post_path(params[:post_id])
     else
-      @post = Post.find(params[:post_id])
       render 'posts/show'
     end
   end
